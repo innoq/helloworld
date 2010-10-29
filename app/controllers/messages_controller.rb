426003276f377593fc6feaea25c7391c168b251c
Till Schulte-Coerne <till.schulte-coerne@innoq.com>
2010-10-29 14:55:18 +0200

    Fixed a bug I produced in messages_controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,8 +1,7 @@
 class MessagesController < ApplicationController
-  before_filter :setup_context
 
   def index
-    @messages = Message.where(:to_id => @profile.id).paginate :page => params[:page], 
+    @messages = Message.where(:to_id => current_user.profile.id).paginate :page => params[:page],
                                                            :per_page => 5,
                                                            :order => 'updated_at DESC'
   end
