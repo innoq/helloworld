8294d3cab5ce616f19366d112be7518e3a67948c
Till Schulte-Coerne <till.schulte-coerne@innoq.com>
2010-11-04 15:19:24 +0100

    Extract speaker names and images from wjax homepage

---
+++ data/extract_speakers.rb
@@ -0,0 +1,27 @@
+require 'rubygems'
+require 'hpricot'
+require 'httpclient'
+
+URL = 'http://jax.de/wjax2010/speaker/'
+
+doc = Hpricot(File.open("speaker.html"))
+
+res = []
+doc.search(".speaker").each do |speaker|
+  s = {}
+  speaker.search("h3 a").remove # remove links and other stuff in the headline
+  s['company'] = speaker.search("h3 span").remove.inner_html.strip
+  s['name'] = speaker.search("h3").inner_html.strip
+  s['name'] =~ /((Dr.|Prof.)\s*)*(.+)/
+  s['first_name'], s['last_name'] = $3.split(/\s/, 2)
+  s['image_url'] = speaker.search("span.pic img").attr('src')
+  file_name = File.expand_path(File.join("photos/speaker/", File.basename(s['image_url'])))
+  File.open(file_name, "w") do |file|
+    file.write HTTPClient.new.get_content(s['image_url'])
+  end
+  s['file'] = file_name
+  res << s
+end
+
+puts res.to_yaml
+
