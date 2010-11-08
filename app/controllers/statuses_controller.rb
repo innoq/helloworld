d0111c493663d955369f0e745e5bc444ad927d3e
Till Schulte-Coerne <till.schulte-coerne@innoq.com>
2010-11-08 18:35:41 +0100

    Added caching headers to /statuses

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -1,11 +1,18 @@
 class StatusesController < ApplicationController
 
+  skip_before_filter :check_login, :only => [:index]
+
   def index
     authorize! :show, Status
 
-    @statuses =  Status.order(Status.arel_table[:created_at].desc).
-      includes(:profile).
-      limit(25)
+    newest_status = Status.order(Status.arel_table[:created_at].desc).first
+
+    if stale?(:etag => newest_status.id, :last_modified => newest_status.created_at)
+      @statuses =  Status.order(Status.arel_table[:created_at].desc).
+        includes(:profile).
+        limit(25)
+      response.headers["expires"] = 1.hour.since.httpdate
+    end
   end
 
   def create
