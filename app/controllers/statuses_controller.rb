134695256ed35663ad3c95c510e90353c5f1085f
Till Schulte-Coerne <till.schulte-coerne@innoq.com>
2010-11-11 17:04:28 +0100

    Fixed error handling in status list

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -1,3 +1,5 @@
+require 'net/http/purge'
+    
 class StatusesController < ApplicationController
 
   skip_before_filter :check_login, :only => [:index]
@@ -6,14 +8,18 @@ class StatusesController < ApplicationController
     authorize! :show, Status
 
     newest_status = Status.order(Status.arel_table[:created_at].desc).first
-
-    sleep(0.5)
-
     if stale?(:etag => newest_status.id, :last_modified => newest_status.created_at)
+
+      sleep(0.5)
+
       @statuses =  Status.order(Status.arel_table[:created_at].desc).
         includes(:profile).
         limit(25)
-      expires_in 1.hour, :private => false
+
+      if !params[:error]
+        expires_in 1.hour, :private => false
+      end
+      
     end
   end
 
@@ -22,14 +28,15 @@ class StatusesController < ApplicationController
         
     @status = Status.create(params[:status])
     @status.profile_id = current_user.profile.id
-    if (!@status.save)
-      flash[:error] = "Could not publish your status update"
-    else
-      require 'net/http/purge'
-      Net::HTTP.new("localhost", "8080").request(Net::HTTP::Purge.new(statuses_path), "")
-    end
 
-    redirect_to statuses_url
+    if (@status.save)
+      Net::HTTP.new("localhost", "8080").request(Net::HTTP::Purge.new(statuses_path), "")
+      redirect_to statuses_url # This one is cached
+    else
+      flash[:error] = "Could not publish your status update"
+      redirect_to statuses_url(:error => 1)
+    end
+    
   end
 
 end
