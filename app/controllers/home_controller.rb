a6ff45bafeb6b2598c9d6e7a64fd64994ced138d
Till Schulte-Coerne <till.schulte-coerne@innoq.com>
2010-11-11 18:03:24 +0100

    Updates of my contacts in dashboard

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,12 @@ class HomeController < ApplicationController
 
   skip_before_filter :check_login, :only => [:about, :header]
 
-  def index
-
+  def dashboard
+    # Load status list
+    @statuses =  Status.order(Status.arel_table[:created_at].desc).
+      includes(:profile => :incoming_relations).
+      where(Relation.arel_table[:source_id].eq(current_user.profile.id)).
+      limit(10)
   end
 
   def header
