afdca044ab457dc7e398252c24358aa293d52ee9
Till Schulte-Coerne <till.schulte-coerne@innoq.com>
2014-02-18 17:06:50 +0100

    Deleted empty helpers

--- base/app/helpers/home_helper.rb
+++
@@ -1,7 +0,0 @@
-module HomeHelper
-  def contact_data(profile, name)
-    if profile.send(name)
-      render "contact_data", :label => name.humanize, :value => profile.send(name).value
-    end
-  end
-end
--- base/app/helpers/routes_helper.rb
+++
@@ -1,7 +0,0 @@
-module RoutesHelper
-
-  def statuses_url
-    "http://localhost:3001/"
-  end
-
-end
