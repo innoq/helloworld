b644859db5bfd89bd806ee5ca61ea154925eb11f
Till Schulte-Coerne <till.schulte-coerne@innoq.com>
2014-02-20 10:26:54 +0100

    Using new login token login
    See d7527d1eeb1586929f71839024e5786198ab6508 for more details

--- network/app/controllers/application_controller.rb
+++ network/app/controllers/application_controller.rb
@@ -9,12 +9,12 @@ class ApplicationController < ActionController::Base
 
   def check_login
     if params[:login_token]
-      uri = foreign_uri('http://helloworld.innoq.com/check_login', :login_token => params[:login_token])
+      uri = foreign_uri('http://helloworld.innoq.com/check_login_token', :login_token => params[:login_token])
       if uri
         uri = URI.parse(uri)
-        Net::HTTP.start(base_uri.host, base_uri.port) do |http|
-          req = Net::HTTP::Get.new(base_uri.request_uri)
-          req['Accept'] = "application/json-home"
+        Net::HTTP.start(uri.host, uri.port) do |http|
+          req = Net::HTTP::Get.new(uri.request_uri)
+          req['Accept'] = "application/json"
           res = http.request(req)
           user = JSON.parse(res.body)
           session[:user] = user["id"]
@@ -24,6 +24,7 @@ class ApplicationController < ActionController::Base
         end
       elsif Rails.env.development?
         session[:user] = User.first.id
+        flash[:info] = "Check Login disabled. Using random user for development purposes."
       else
         render :text => "Login currently disabled"
       end
@@ -33,7 +34,7 @@ class ApplicationController < ActionController::Base
   end
 
   def current_user
-    @current_user ||= session[:user].nil? ? nil : User.includes(:profile => :contacts).where(:id => session[:user]).last
+    @current_user ||= session[:user].nil? ? nil : User.includes(:profile => {:relations => :destination}).where(:id => session[:user]).last
   end
 
   def foreign_uri(resource, params = {})
