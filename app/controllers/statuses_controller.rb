e19c10e15e9eadd69f51a5bbfec6edfd8c4ec1ed
Till Schulte-Coerne <till.schulte-coerne@innoq.com>
2017-11-06 10:31:01 +0100

    allow cache-control: max-age too

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -4,7 +4,7 @@ class StatusesController < ApplicationController
 
   skip_before_filter :check_login, :only => [:index]
   skip_before_filter :verify_authenticity_token
-  
+
   def index
     authorize! :show, Status
 
@@ -16,8 +16,13 @@ class StatusesController < ApplicationController
       includes(:profile).
       limit(25)
 
+    # # Set `Cache-Control: max-age=1200, public`
+    # expires_in 1.hour, :public => true
+
+    # # Alternatively:
     # # Force Varnish to store this without touching the Cache-Control header (which is for the Client)
     # response.headers['X-Reverse-Proxy-ttl'] = '120'
+
 
     # # Register this to Varnish XKey tags
     # response.headers['xkey'] = 'statuslist'
